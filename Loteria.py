a4a95111fad0f1c586f8a59df51dc9dc336bbc22
Mateus Nepomuceno <mateusnepomuceno2000@gmail.com>
2025-07-23 22:14:11 -0300

    refactor: mudança no number_input

--- Loteria.py
+++ Loteria.py
@@ -4,16 +4,21 @@ import random as rd
 def novo_sorteio():
             st.session_state.sorteio = rd.randint(1,15)
             st.session_state.chances = 3
-            st.session_state.usuario = None
 
 def verificar_sorteio():
-    if 1 <= st.session_state.usuario <=15:
-        if st.session_state.usuario == st.session_state.sorteio:
+    usuario = st.session_state.get("usuario", 0)
+
+    if usuario is None:
+        st.error("Por favor, insira um número válido!")
+        return
+    
+    if 1 <= usuario <=15:
+        if usuario == st.session_state.sorteio:
             st.success("Parabéns, você acertou o número!")
             st.balloons()
             st.session_state.chances = 0
 
-        elif st.session_state.usuario > st.session_state.sorteio:
+        elif usuario > st.session_state.sorteio:
             st.session_state.chances -=1
             if st.session_state.chances > 0:
                 st.warning("O número é menor.")
@@ -25,8 +30,6 @@ def verificar_sorteio():
                 st.warning("O número é maior.")
             else:
                 st.error(f"Suas tentativas acabaram! O número era {st.session_state.sorteio}")
-    else:
-         return
 
 if 'sorteio' not in st.session_state:
     novo_sorteio() 
@@ -36,12 +39,13 @@ st.write(f"Você tem {st.session_state.chances} chances para acertar o número."
 
 desativar = st.session_state.chances == 0
 
-st.session_state.usuario = st.number_input(
+st.number_input(
     "Entre com um valor de 1 até 15:",
     min_value=1,
     max_value=15,
-    value=1,
-    disabled=desativar
+    value=None,
+    disabled=desativar,
+    key="usuario"
 )    
 
 a, b,_ = st.columns([1.5, 2, 8])
